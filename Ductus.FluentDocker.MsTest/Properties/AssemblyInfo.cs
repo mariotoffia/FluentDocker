3fda3a3a14a792f5b7c7e87911d4ddafad365c4e
Mario Toffia <mario.toffia@dataductus.se>
2016-03-23 23:02:27 +0100

    Added new nuget client and updated assembly version

--- Ductus.FluentDocker.MsTest/Properties/AssemblyInfo.cs
+++ Ductus.FluentDocker.MsTest/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("871473b8-75e1-4265-a679-99ca6971e2f0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
