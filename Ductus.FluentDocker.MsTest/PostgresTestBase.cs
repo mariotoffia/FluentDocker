5b9696a4164ded672465ad8960c6bc0d53190c9d
Mario Toffia <mario.toffia@dataductus.se>
2016-03-23 21:50:18 +0100

    Added unittest for postgres ms test base.

--- Ductus.FluentDocker.MsTest/PostgresTestBase.cs
+++ Ductus.FluentDocker.MsTest/PostgresTestBase.cs
@@ -2,7 +2,7 @@
 {
   public abstract class PostgresTestBase : FluentDockerTestBase
   {
-    private const string PostgresConnectionString =
+    protected const string PostgresConnectionString =
       "Server={0};Port={1};Userid={2};Password={3};" +
       "Pooling=false;MinPoolSize=1;MaxPoolSize=20;" +
       "Timeout=15;SslMode=Disable;Database={4}";
---
+++ Ductus.FluentDockerTest/PostgresMsTests.cs
@@ -0,0 +1,18 @@
+﻿using Ductus.FluentDocker.MsTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ductus.FluentDockerTest
+{
+  [TestClass]
+  public class PostgresMsTests : PostgresTestBase
+  {
+    [TestMethod]
+    public void RunningContainerAndConnectionStringSetWithinTestMethod()
+    {
+      Assert.IsNotNull(Container);
+      Assert.AreEqual(string.Format(PostgresConnectionString, Container.Host,
+        Container.GetHostPort("5432/tcp"), PostgresUser,
+        PostgresPassword, PostgresDb), ConnectionString);
+    }
+  }
+}
