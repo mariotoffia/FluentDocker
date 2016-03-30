eb16302db808f7e601a06b04c9e4ab6480e01fd2
Mario Toffia <mario.toffia@dataductus.se>
2016-03-30 22:09:24 +0200

    Bump version to 1.1.2

--- Ductus.FluentDocker/Properties/AssemblyInfo.cs
+++ Ductus.FluentDocker/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Ductus.FluentDocker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fluent interface to create, run and manage docker containers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Data Ductus")]
+[assembly: AssemblyCompany("Data Ductus Malmö AB")]
 [assembly: AssemblyProduct("Ductus.FluentDocker")]
 [assembly: AssemblyCopyright("Copyright ©  2016 Mario Toffia")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4ae9f635-351c-4c47-93f3-a9fad18f122f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
--- Ductus.FluentDockerTest/Properties/AssemblyInfo.cs
+++ Ductus.FluentDockerTest/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Ductus.FluentDockerTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base classes to use when running unit-test using docker containers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Data Ductus Malmö AB")]
 [assembly: AssemblyProduct("Ductus.FluentDockerTest")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Mario Toffia")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03e0dd1c-f964-4993-b8f9-9a072398bbbc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
