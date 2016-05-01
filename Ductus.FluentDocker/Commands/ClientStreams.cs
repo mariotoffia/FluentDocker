634ffbabcd81e8611c6a464c87618cbde623741e
Mario Toffia <mario.toffia@dataductus.se>
2016-05-01 22:15:30 +0200

    Added event streaming for docker client.

--- Ductus.FluentDocker/Commands/ClientStreams.cs
+++ Ductus.FluentDocker/Commands/ClientStreams.cs
@@ -40,5 +40,37 @@ namespace Ductus.FluentDocker.Commands
           "docker".ResolveBinary(),
           $"{args} logs {options} {id}").Execute(cancellationToken);
     }
+
+    public static ConsoleStream<string> Events(this DockerUri host,
+      CancellationToken cancellationToken = default(CancellationToken),
+      string []filters = null, DateTime ?since = null, DateTime? until = null,
+      ICertificatePaths certificates = null)
+    {
+      var args = $"{host.RenderBaseArgs(certificates)}";
+
+      var options = string.Empty;
+      if (null != since)
+      {
+        options += $" --since {since}";
+      }
+
+      if (null != until)
+      {
+        options += $" --since {until}";
+      }
+
+      if (null != filters && 0 != filters.Length)
+      {
+        foreach (var filter in filters)
+        {
+          options += $" --filter={filter}";
+        }
+      }
+
+      return
+        new StreamProcessExecutor<StringMapper, string>(
+          "docker".ResolveBinary(),
+          $"{args} events {options}").Execute(cancellationToken);
+    }
   }
 }
