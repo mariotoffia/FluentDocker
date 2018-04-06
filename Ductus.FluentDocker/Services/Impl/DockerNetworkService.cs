97bdaf6c8431863c4cd04afdb11538ad00e87d36
Mario Toffia <mario.toffia@dataductus.se>
2018-04-06 09:29:54 +0200

    Added multi reference - it now supports standard 1.1 and 2.0 (and old 4.5 framrwork) at the same time.

---
+++ Ductus.FluentDocker/Services/Impl/DockerNetworkService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ductus.FluentDocker.Services.Impl
+{
+    public sealed class DockerNetworkService : ServiceBase
+    {
+      public DockerNetworkService(string name) : base(name)
+      {
+      }
+
+      public override void Dispose()
+      {
+        throw new NotImplementedException();
+      }
+
+      public override void Start()
+      {
+        throw new NotImplementedException();
+      }
+
+      public override void Stop()
+      {
+        throw new NotImplementedException();
+      }
+
+      public override void Remove(bool force = false)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}
