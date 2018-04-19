e8c511f559db100bf448c316514a5fe209362d33
Mario Toffia <mario.toffia@dataductus.se>
2018-04-19 14:14:55 +0200

    Fixed Access to disposed closure in WaitForState.
    The ManualResetEventSlim may be accessesd in a disposed context and therefore get runtime exception.

--- Ductus.FluentDocker/Extensions/ServiceExtensions.cs
+++ Ductus.FluentDocker/Extensions/ServiceExtensions.cs
@@ -7,27 +7,45 @@ namespace Ductus.FluentDocker.Extensions
 {
 	public static class ServiceExtensions
 	{
+	  sealed class WaitForStateObject
+	  {
+	    public ManualResetEventSlim Mre { get; set; }
+      public IContainerService Container { get; set; }
+      public ServiceRunningState State { get; set; }
+      public Exception Exception { get; set; }
+    }
+
 		private static void WaitForState(this IContainerService container, ServiceRunningState state)
 		{
-			Exception exception = null;
-			using (var mre = new ManualResetEventSlim())
-			using (new Timer(_ =>
-			{
-				var containerState = container.GetConfiguration(true).State;
-				if (!string.IsNullOrWhiteSpace(containerState.Error))
-				{
-					exception = new FluentDockerException($"Unable to start container: {containerState.Error}");
-					mre.Set();
-				}
-				if (containerState.ToServiceState() == state)
-					mre.Set();
-			}, null, 0, 500))
-				mre.Wait();
-			if (exception != null)
-				throw exception;
+		  WaitForStateObject so;
+      using (var mre = new ManualResetEventSlim())
+		  {
+		    so = new WaitForStateObject { Mre = mre, Container = container, State = state };
+        using (new Timer(Callback, so, 0, 500))
+		    {
+		      mre.Wait();
+        }
+      }
+
+      if (so.Exception != null)
+				throw so.Exception;
 		}
 
-		public static void WaitForRunning(this IContainerService container) =>
+	  private static void Callback(object state)
+	  {
+	    var obj = (WaitForStateObject) state;
+	    var containerState = obj.Container.GetConfiguration(true).State;
+	    if (!string.IsNullOrWhiteSpace(containerState.Error))
+	    {
+	      obj.Exception = new FluentDockerException($"Unable to start container: {containerState.Error}");
+	      obj.Mre.Set();
+	    }
+	    if (containerState.ToServiceState() == obj.State)
+	      obj.Mre.Set();
+
+    }
+
+    public static void WaitForRunning(this IContainerService container) =>
 			WaitForState(container, ServiceRunningState.Running);
 
 		public static void WaitForStopped(this IContainerService container) =>
