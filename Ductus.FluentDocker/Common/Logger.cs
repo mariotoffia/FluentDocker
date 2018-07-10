4a6f54bc3d76990077970984cf59f6fc994f1211
Mario Toffia <mario.toffia@dataductus.se>
2018-07-10 23:23:03 +0200

    Added support to programmatically forcefully enable / disable logging.
    This is usefull when the normal configuration files are not wanted and just a simple way of disabling logging. It is turned on by default to be backwards compatible. This is related to issue #49

--- Ductus.FluentDocker/Common/Logger.cs
+++ Ductus.FluentDocker/Common/Logger.cs
@@ -1,32 +1,35 @@
 ﻿using System;
-
 #if COREFX
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
 #endif
 
 namespace Ductus.FluentDocker.Common
 {
-	internal static class Logger
-	{
-		public static void Log(string message)
-		{
+  internal static class Logger
+  {
+    internal static bool Enabled = true;
+
+    public static void Log(string message)
+    {
+      if (!Enabled) return;
+
 #if COREFX
-			ILogger.Value.LogTrace(message);
-		}
+      ILogger.Value.LogTrace(message);
+    }
 
 
-
-		private static readonly Lazy<ILogger> ILogger
-			= new Lazy<ILogger>(() =>
-			{
-				var factory = new ServiceCollection().AddLogging().BuildServiceProvider().GetRequiredService<ILoggerFactory>();
-				factory.AddDebug();
-				return factory.CreateLogger(Constants.DebugCategory);
-			});
+    private static readonly Lazy<ILogger> ILogger
+      = new Lazy<ILogger>(() =>
+      {
+        var factory = new ServiceCollection().AddLogging().BuildServiceProvider().GetRequiredService<ILoggerFactory>();
+        factory.AddDebug();
+        return factory.CreateLogger(Constants.DebugCategory);
+      });
 #else
 			System.Diagnostics.Debugger.Log((int)System.Diagnostics.TraceLevel.Verbose, Constants.DebugCategory, message);
 		}
 #endif
-	}
+  }
 }
---
+++ Ductus.FluentDocker/Services/Logging.cs
@@ -0,0 +1,26 @@
+﻿using Ductus.FluentDocker.Common;
+
+namespace Ductus.FluentDocker.Services
+{
+  /// <summary>
+  ///   Controls Logging.
+  /// </summary>
+  public static class Logging
+  {
+    /// <summary>
+    ///   Enables logging.
+    /// </summary>
+    public static void Enabled()
+    {
+      Logger.Enabled = true;
+    }
+
+    /// <summary>
+    ///   Disables logging.
+    /// </summary>
+    public static void Disabled()
+    {
+      Logger.Enabled = false;
+    }
+  }
+}
