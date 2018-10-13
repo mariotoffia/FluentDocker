2412cd73ae71f90cc2148ac4b4eddcf76cb63be4
Mario Toffia <mario.toffia@dataductus.se>
2018-10-13 08:01:55 +0200

    Restructuring mstest into separate features.

---
+++ FluentDocker.Features/FluentDocker.Features.Elk/ElkFeature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Ductus.FluentDocker.Model;
+
+namespace FluentDocker.Features.Elk
+{
+  // https://github.com/libgit2/libgit2sharp/tree/master/LibGit2Sharp.Tests
+  public class ElkFeature : IFeature
+  {
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+
+    public string Id { get; }
+    public void Initialize(IDictionary<string, string> settings = null)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Execute(params string[] arguments)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
