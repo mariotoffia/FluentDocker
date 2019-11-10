fd31b90b1e797aefe59d1f8df0ed468f50610b98
Mario Toffia <mario.toffia@dataductus.se>
2019-11-10 18:02:01 +0100

    Created base host event , type, and action

---
+++ Ductus.FluentDocker/Model/HostEvents/Action.cs
@@ -0,0 +1,16 @@
+namespace Ductus.FluentDocker.Model.HostEvents
+{
+  public enum Action
+  {
+    Unknown,
+    Pull,
+    Create,
+    Start,
+    Kill,
+    Die,
+    Connect,
+    Disconnect,
+    Stop,
+    Destroy
+  }
+}
---
+++ Ductus.FluentDocker/Model/HostEvents/HostEventType.cs
@@ -0,0 +1,17 @@
+namespace Ductus.FluentDocker.Model.HostEvents
+{
+  public enum EventType
+  {
+    Unknonwn,
+    Image,
+    Container,
+    Network,
+    Plugin,
+    Volume,
+    Daemon,
+    Service,
+    Node,
+    Secret,
+    Config
+  }
+}
