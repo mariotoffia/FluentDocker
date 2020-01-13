5753ed084c6435342512fe62bbaca92dcc645da8
Mario Toffia <mario.toffia@dataductus.se>
2020-01-13 14:34:50 +0100

    reformatted (again) process executor

--- Ductus.FluentDocker/Executors/ProcessExecutor.cs
+++ Ductus.FluentDocker/Executors/ProcessExecutor.cs
@@ -9,8 +9,8 @@ namespace Ductus.FluentDocker.Executors
 {
   public sealed class ProcessExecutor<T, TE> where T : IProcessResponseParser<TE>, IProcessResponse<TE>, new()
   {
-    private readonly string _command;
     private readonly string _arguments;
+    private readonly string _command;
     private readonly string _workingdir;
 
     public ProcessExecutor(string command, string arguments, string workingdir = null)
@@ -44,44 +44,33 @@ namespace Ductus.FluentDocker.Executors
       };
 
       if (0 != Env.Count)
-      {
         foreach (var key in Env.Keys)
         {
 #if COREFX
           startInfo.Environment[key] = Env[key];
 #else
-          startInfo.EnvironmentVariables[key] = Env[key]; 
+          startInfo.EnvironmentVariables[key] = Env[key];
 #endif
         }
-      }
 
       Logger.Log($"cmd: {_command} - arg: {_arguments}");
 
-      using (var process = new Process { StartInfo = startInfo })
+      using (var process = new Process {StartInfo = startInfo})
       {
         var output = new StringBuilder();
         var err = new StringBuilder();
 
         process.OutputDataReceived += (sender, args) =>
         {
-          if (!string.IsNullOrEmpty(args.Data))
-          {
-            output.AppendLine(args.Data);
-          }
+          if (!string.IsNullOrEmpty(args.Data)) output.AppendLine(args.Data);
         };
 
         process.ErrorDataReceived += (sender, args) =>
         {
-          if (!string.IsNullOrEmpty(args.Data))
-          {
-            err.AppendLine(args.Data);
-          }
+          if (!string.IsNullOrEmpty(args.Data)) err.AppendLine(args.Data);
         };
 
-        if (!process.Start())
-        {
-          throw new FluentDockerException($"Could not start process {_command}");
-        }
+        if (!process.Start()) throw new FluentDockerException($"Could not start process {_command}");
 
         process.BeginOutputReadLine();
         process.BeginErrorReadLine();
