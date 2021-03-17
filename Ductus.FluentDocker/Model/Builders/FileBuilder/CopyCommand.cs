8460e6ff2ae4a3c34fc1e664ce0f3074d4c0deb4
Mario Toffia <mario.toffia@gmail.com>
2021-03-17 09:59:20 +0100

    Copy and from enhanced as per Issue #178

--- Ductus.FluentDocker/Model/Builders/FileBuilder/CopyCommand.cs
+++ Ductus.FluentDocker/Model/Builders/FileBuilder/CopyCommand.cs
@@ -1,19 +1,54 @@
-﻿namespace Ductus.FluentDocker.Model.Builders.FileBuilder
+﻿using Ductus.FluentDocker.Extensions;
+using Ductus.FluentDocker.Model.Common;
+
+namespace Ductus.FluentDocker.Model.Builders.FileBuilder
 {
   public sealed class CopyCommand : ICommand
   {
-    public CopyCommand(string from, string to)
+    /// <summary>
+    /// This generates the _COPY_ command.
+    /// </summary>
+    /// <param name="from">From directory.</param>
+    /// <param name="to">To directory.</param>
+    /// <param name="chownUserAndGroup">Optional --chown user:group.</param>
+    /// <param name="fromAlias">
+    /// Optional source location from earlier buildstage FROM ... AS alias. This will 
+    /// generate --from=aliasname in the _COPY_ command and hence reference a earlier
+    /// _FROM ... AS aliasname_ buildstep as source.
+    /// </param>
+    public CopyCommand(TemplateString from, TemplateString to, 
+      TemplateString chownUserAndGroup = null, TemplateString fromAlias = null)
     {
-      From = from;
-      To = to;
+      From = from.Rendered.WrapWithChar("\"");
+      To = to.Rendered.WrapWithChar("\"");
+
+      if (null != chownUserAndGroup && !string.IsNullOrEmpty(chownUserAndGroup.Rendered)) {
+        Chown = chownUserAndGroup.Rendered;
+      }
+
+      if (null != fromAlias && !string.IsNullOrEmpty(fromAlias.Rendered)) {
+        Alias = fromAlias.Rendered;
+      }
     }
 
     public string From { get; }
     public string To { get; }
+    public string Alias { get; }
+    public string Chown { get; }
 
     public override string ToString()
     {
-      return $"COPY {From} {To}";
+      string s = "COPY";
+
+      if (!string.IsNullOrEmpty(Chown)) {
+        s = $"{s} --chown={Chown}";
+      }
+
+      if (!string.IsNullOrEmpty(Alias)) {
+        s = $"{s} --from={Alias}";
+      }
+
+      return $"{s} [{From},{To}]";
     }
   }
 }
--- Ductus.FluentDocker/Model/Builders/FileBuilder/FromCommand.cs
+++ Ductus.FluentDocker/Model/Builders/FileBuilder/FromCommand.cs
@@ -1,17 +1,54 @@
-﻿namespace Ductus.FluentDocker.Model.Builders.FileBuilder
+﻿using Ductus.FluentDocker.Common;
+using Ductus.FluentDocker.Model.Common;
+
+namespace Ductus.FluentDocker.Model.Builders.FileBuilder
 {
   public sealed class FromCommand : ICommand
   {
-    public FromCommand(string @from)
+    /// <summary>
+    /// Specifies the _FROM_ command.
+    /// </summary>
+    /// <param name="imageAndTag">The image to derive from and a optional (colon) tag, e.g. myimg:mytag</param>
+    /// <param name="asName">An optional alias.</param>
+    /// <param name="platform">An optional platform such linux/amd64 or windows/amd64.</param>
+    public FromCommand(TemplateString imageAndTag, TemplateString asName = null, TemplateString platform = null)
     {
-      From = @from;
+      if (null == imageAndTag || string.IsNullOrEmpty(imageAndTag.Rendered)) {
+        throw new FluentDockerException("FROM requires atleast a image name");
+      }
+
+      ImageAndTag = imageAndTag;
+
+      if (null != asName && !string.IsNullOrEmpty(asName.Rendered))
+      {
+        Alias = asName.Rendered;
+      }
+
+      if (null != platform && !string.IsNullOrEmpty(platform.Rendered))
+      {
+        Platform = platform.Rendered;
+      }
     }
 
-    public string From { get; }
+    public string ImageAndTag { get; }
+    public string Platform { get; }
+    public string Alias { get; }
 
     public override string ToString()
     {
-      return $"FROM {From}";
+      var s = "FROM";
+
+      if (!string.IsNullOrEmpty(Platform)) {
+        s = $"{s} --platform={Platform}";
+      }
+
+      s = $"{s} {ImageAndTag}";
+
+      if (!string.IsNullOrEmpty(Alias)) {
+        s = $"{s} {Alias}";
+      }
+
+      return s;
     }
   }
 }
