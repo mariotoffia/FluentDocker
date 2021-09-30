a9189c79fad7b7b37e2677a2a615d164d87dcebb
jgozner <32521239+jgozner@users.noreply.github.com>
2021-09-30 14:46:03 -0700

    XUnit base classes

---
+++ Ductus.FluentDocker.XUnit/PostgresTestBase.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services.Extensions;
+
+namespace Ductus.FluentDocker.XUnit
+{
+  public abstract class PostgresTestBase : FluentDockerTestBase
+  {
+    protected const string PostgresConnectionString =
+      "Server={0};Port={1};Userid={2};Password={3};" +
+      "Pooling=false;MinPoolSize=1;MaxPoolSize=20;" +
+      "Timeout=15;SslMode=Disable;Database={4}";
+
+    protected const string PostgresUser = "postgres";
+    protected const string PostgresDb = "postgres";
+    protected readonly string DockerImage;
+    protected readonly string PostgresPassword;
+
+    protected string ConnectionString;
+
+    protected PostgresTestBase(string password = "mysecretpassword", string image = "kiasaki/alpine-postgres")
+    {
+      PostgresPassword = password;
+      DockerImage = image;
+    }
+
+    protected override ContainerBuilder Build()
+    {
+      return new Builder().UseContainer()
+        .UseImage("kiasaki/alpine-postgres")
+        .WithEnvironment($"POSTGRES_PASSWORD={PostgresPassword}")
+        .ExposePort(5432)
+        .WaitForPort("5432/tcp", 30000 /*30s*/);
+    }
+
+    protected override void OnContainerInitialized()
+    {
+      var ep = Container.ToHostExposedEndpoint("5432/tcp");
+      ConnectionString = string.Format(PostgresConnectionString, ep.Address, ep.Port, PostgresUser,
+        PostgresPassword, PostgresDb);
+    }
+  }
+}
