1dd0dd66699eb5b90dd31c5258a75eb49b86c532
Rory Reid <rory.roberts@outlook.com>
2023-09-28 21:58:31 +0100

    Change Container.State.Health.Status to be nullable
    The only place this is used (.WaitForHealthy()) already performs a null-
    safe check against the Status, comparing it to "Healthy" only. Changing
    the property to nullable allows more defensive parsing if it is an empty
    string for whatever reason, and is still interpreted as "Unhealthy"
    where it matters.

--- Ductus.FluentDocker/Model/Containers/Health.cs
+++ Ductus.FluentDocker/Model/Containers/Health.cs
@@ -2,7 +2,7 @@ namespace Ductus.FluentDocker.Model.Containers
 {
   public class Health
   {
-    public HealthState Status { get; set; }
+    public HealthState? Status { get; set; }
     public int FailingStreak { get; set; }
   }
 }
